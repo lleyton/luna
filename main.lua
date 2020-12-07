d8a267eb328864ef040c9696a727defbab6f05d1
Lleyton Gray <lleyton@innatical.com>
2020-12-06 23:35:22 -0800

    Some require bs

--- main.lua
+++ main.lua
@@ -1,5 +1,15 @@
 #!/usr/bin/env lua
-package.path = [[luarocks-api/src/?.lua;]] .. package.path
+
+-- https://stackoverflow.com/questions/11714204/is-it-possible-to-make-imports-depend-on-the-location-of-my-lua-script-instead-o
+local root = arg and arg[0]
+if root then
+  local path = root:match [[^(.+[\/])[^\/]+$]]
+  if path and #path > 0 then
+    package.path = path..'luarocks-api/src/?.lua;'..package.path
+    package.path = path .. '?.lua;'.. package.path
+  end
+end
+
 local luarocks = require 'luarocks'
 luarocks.setup()
 
