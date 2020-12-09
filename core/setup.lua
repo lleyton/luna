7506e0e10656d2d59184e69980309fa421460d6f
Lleyton Gray <lleyton@innatical.com>
2020-12-09 00:36:03 -0800

    Add support for variables

--- core/setup.lua
+++ core/setup.lua
@@ -1,7 +1,13 @@
 local luarocks = require 'luarocks'
 local config = require 'core.config'
+local luarocks_cfg = require 'luarocks.core.cfg'
 
 return function(version)
   local cfg = config.read(os.getenv('HOME') .. '/.lunarc.toml')
   luarocks.setup(cfg.versions[version])
+  if cfg.variables then
+    for k, v in pairs(cfg.variables) do
+      luarocks_cfg.variables[k] = v
+    end
+  end
 end
