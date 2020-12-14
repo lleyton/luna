33cc94ebd5722e69b921fdf28ca2c23d7f605de8
Lleyton Gray <lleyton@innatical.com>
2020-12-14 11:19:15 -0800

    Fix init command

--- commands/init.lua
+++ commands/init.lua
@@ -5,11 +5,10 @@ parser:command('init', 'Creates a new luna.toml'):action(function()
   config.write('luna.toml', {
     package = {
       name = 'my-new-project',
-      version = '0.1.0'
-    },
-    dependencies = {
+      version = '0.1.0',
       lua = '5.4'
     },
+    dependencies = {},
     scripts = {
       start = 'lua main.lua'
     }
